0b47f88f6a9a1a9abd6d95f987198bd46a841543
Theo Stevens <theo.stevens03@gmail.com>
2017-10-13 21:02:58 +0100

    Fixed/improved a few things

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const browserify = require('browserify');
+const http = require('http');
+const port = (process.env.NODE_ENV || 3000);
 
 var app = express();
 
@@ -6,20 +9,35 @@ var app = express();
 app.set('view engine', 'pug');
 
 
+//Logs every request, for debugging
 app.use(function(req,res,next){
   console.log(req.method, ' ',req.url); next();
 });
 
+
+//This is an expensive operation, make sure NODE_ENv is "production" for deployment
+//Speeds up development, though, since you can rebuild React on the fly
+if(!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
+  app.use('/scripts/bundle.js', function(req, res){
+    browserify("./react-app/app.js")
+      .transform("babelify", {presets: ["es2015", "react"]})
+      .bundle()
+      .pipe(res);
+  })
+}
+
+
+//Serve static files from public
 app.use(express.static('public'));
 
-
+//Change the '/' to '*' if you want to do ui routing at the front-end
 app.get('/', function(req, res, next) {
   res.render('index');
 });
 
 
-//init server, will use PORT environment variable if available, for Heroku deployment
-var server = app.listen(process.env.PORT || 3000, function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
+//Initialising server w/ Express handling requests
+var server = http.createServer(app);
+server.listen(port);
+
+console.log('Ready to go, yo, on ' + port);
