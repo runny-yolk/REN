4dea6361e4969a4a4220469953a5ef555f008a23
Theo Stevens <theo.stevens03@gmail.com>
2017-11-21 21:19:23 +0000

    Added a few extra bits, to be useful for tutorials

--- react-app/app.js
+++ react-app/app.js
@@ -1,11 +1,38 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+// Presentational component
+const Hello = function(props){
+    return(
+        <div>
+            {'Hello, '+props.place+'!'}
+        </div>
+    )
+}
+
+// Compositional component
 class Page extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            place: 'World'
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState({
+            place: 'Kentucky'
+        })
+    }
+
     render(){
         return(
             <div>
-                Hello, world!
+                <input type="button" onClick={this.handleClick} value="Change place"/>
+                <Hello place={this.state.place} />
             </div>
         )
     }
@@ -13,6 +40,6 @@ class Page extends React.Component {
 
 
 ReactDOM.render(
-    <Page/>,
+    <Page />,
     document.getElementById('root')
 );
